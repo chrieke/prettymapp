2322adc0d9a31cc48be03a7dbbfab2df702022ed
Christoph Rieke <christoph.k.rieke@gmail.com>
2021-12-28 22:42:31 +0100

    establish live tests

--- prettiermaps/tests/test_geo.py
+++ prettiermaps/tests/test_geo.py
@@ -1,7 +1,9 @@
+from mock import patch
+
 import pytest
 from shapely.geometry import Polygon, Point
 from geopandas import GeoDataFrame
-
+import osmnx as ox
 
 from prettiermaps import geo
 
@@ -15,14 +17,26 @@ def test_validate_coordinates():
         geo.validate_coordinates(lat=92.3, lon=-237.2)
 
 
-def test_get_aoi_from_user_input():
+@patch.object(ox, "geocode")
+def test_get_aoi_from_user_input(ox_geocode):
+    ox_geocode.return_value = 52.52, 13.4
+
     poly = geo.get_aoi_from_user_input("Unter den Linden 37, 10117 Berlin")
     assert isinstance(poly, Polygon)
     poly = geo.get_aoi_from_user_input(coordinates=(52.52, 13.4))
     assert isinstance(poly, Polygon)
 
 
-def test_query_osm_data():
+@pytest.mark.live
+def test_get_aoi_from_user_input_live():
+    poly = geo.get_aoi_from_user_input("Unter den Linden 37, 10117 Berlin")
+    assert isinstance(poly, Polygon)
+    poly = geo.get_aoi_from_user_input(coordinates=(52.52, 13.4))
+    assert isinstance(poly, Polygon)
+
+
+@pytest.mark.live
+def test_query_osm_data_live():
     aoi = Point(13.380972146987915, 52.51517622886228).buffer(0.001)
     df = geo.query_osm_data(aoi=aoi)
     isinstance(df, GeoDataFrame)
