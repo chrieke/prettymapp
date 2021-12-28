946b528eda4b25fce65146e1accdf052a1f0d6aa
Christoph Rieke <christoph.k.rieke@gmail.com>
2021-12-28 23:17:35 +0100

    main

--- prettiermaps/main.py
+++ prettiermaps/main.py
@@ -1,22 +1,11 @@
-# from typing import Tuple
-#
-# import osmnx as ox
-# import geopandas as gpd
-# from shapely.geometry import Polygon, Point
-#
-#
-# def adjust_street_geometries():
-#     pass
-#     # Adjust element width
-#     # streets_width = {
-#     # 	'motorway': 5,
-#     # 	'trunk': 5,
-#     # 	'primary': 4.5,
-#     # 	'secondary': 4,
-#     # 	'tertiary': 3.5,
-#     # 	'residential': 3,
-#     # 	'service': 2,
-#     # 	'unclassified': 2,
-#     # 	'pedestrian': 2,
-#     # 	'footway': 1,
-#     # }
+from prettiermaps import geo
+
+
+def main():
+    address = "Praça Ferreira do Amaral, Macau"
+    radius = 1100
+
+    aoi = geo.get_aoi_from_user_input(address=address, radius=radius)
+    df = geo.query_osm_data(aoi=aoi)
+
+    return df
--- prettiermaps/tests/test_main.py
+++ prettiermaps/tests/test_main.py
@@ -1,35 +1,8 @@
-# import geopandas as gpd
-# import osmnx as ox
-# import matplotlib.pyplot as plt
-# from descartes import PolygonPatch
-#
-# from prettiermaps import main
+from prettiermaps import main
 
 
-custom_filters = {
-    "streets": '["highway"~"motorway|trunk|primary|secondary|tertiary|'
-    'residential|service|unclassified|pedestrian|footway"]'
-}
+def test_main():
+    main.main()
 
 
-# def test_query_osm_data():
-#     address = "Praça Ferreira do Amaral, Macau"
-#     radius = 1100
 #
-#     df = query_osm_data(address=address, radius=radius, custom_filters=custom_filters)
-#
-#     # PLOT
-#     fig, ax = plt.subplots(1, 1, figsize=(12, 12))
-#     # ax.axis("off")
-#     # ax.axis("equal")
-#     drawing_kwargs = {"fc": "#2F3737", "ec": "#475657"}
-#     geom = df.dissolve().plot(
-#         ax=ax, **drawing_kwargs
-#     )  # multilinestring #TODO probably multiple mls from gdf
-#     plt.show()
-#
-#     ax.add_patch(PolygonPatch(geom, **drawing_kwargs))
-#
-#     plt.show()
-#
-#     assert isinstance(df, gpd.GeoDataFrame)
