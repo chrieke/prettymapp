73ba93636c924008466b5ad4bbb6b0630d51b5b4
Christoph Rieke <christoph.k.rieke@gmail.com>
2021-12-29 23:55:38 +0100

    rename dataprep

--- prettybasicmaps/main.py
+++ prettybasicmaps/main.py
@@ -3,13 +3,13 @@ import geopandas as gpd
 
 from prettybasicmaps import geo
 from prettybasicmaps import plotting
-from prettybasicmaps import dataclean
+from prettybasicmaps import dataprep
 from prettybasicmaps.settings import LANDCOVER, DRAW_SETTINGS
 
 
 def main(address="Praça Ferreira do Amaral, Macau", radius=1100):
 
-    osm_tags = dataclean.osm_tags_from_settings(settings=LANDCOVER)
+    osm_tags = dataprep.osm_tags_from_settings(settings=LANDCOVER)
 
     # aoi = bbox_to_poly(*bbox_from_point(geocode(query=address), dist=radius)) # Might be faster
     aoi = geo.get_aoi_from_user_input(address=address, radius=radius)
@@ -17,7 +17,7 @@ def main(address="Praça Ferreira do Amaral, Macau", radius=1100):
     df = geometries_from_polygon(polygon=aoi, tags=osm_tags)
     df = gpd.clip(df, aoi)
 
-    df = dataclean.cleanup_df(df=df, landcover=LANDCOVER)
+    df = dataprep.cleanup_df(df=df, landcover=LANDCOVER)
     df = geo.adjust_street_width(df=df)
 
     ax = plotting.plot(df, drawing_kwargs=DRAW_SETTINGS)
