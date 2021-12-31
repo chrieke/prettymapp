9cc677775e25d2308808b8253b0ff747cbdcb712
Christoph Rieke <christoph.k.rieke@gmail.com>
2021-12-31 14:10:35 +0100

    fix overwrite display kwargs

--- prettybasicmaps/plotting.py
+++ prettybasicmaps/plotting.py
@@ -19,13 +19,13 @@ def plot(df, drawing_kwargs):
 
     for lc_class in df["landcover_class"].unique():
         if lc_class == "urban":
-            print("aa", drawing_kwargs[lc_class]["cmap"])
-            drawing_kwargs[lc_class]["cmap"] = ListedColormap(
-                drawing_kwargs[lc_class]["cmap"]
+            urban_drawing_kwargs = drawing_kwargs[lc_class].copy()
+            urban_drawing_kwargs["cmap"] = ListedColormap(
+                urban_drawing_kwargs["cmap"]
             )
             df_urban = df[df["landcover_class"] == lc_class]
             df_urban["randint"] = np.random.randint(0, 3, df_urban.shape[0])
-            df_urban.plot(ax=ax, column="randint", **drawing_kwargs[lc_class])
+            df_urban.plot(ax=ax, column="randint", **urban_drawing_kwargs)
         else:
             df[df["landcover_class"] == lc_class].plot(
                 ax=ax, **drawing_kwargs[lc_class]
