bbbb4afea9309a8fe335f6fd719592da5e642dea
Christoph Rieke <christoph.k.rieke@gmail.com>
2022-02-20 22:07:50 +0100

    move examples to separate module

--- streamlit-prettybasicmaps/app.py
+++ streamlit-prettybasicmaps/app.py
@@ -1,27 +1,10 @@
 import streamlit as st
 
+from examples import EXAMPLES
 from utils import image_button_config, st_get_geometries, st_plot
 from prettybasicmaps.settings import DRAW_SETTINGS
 
-EXAMPLES = {
-    "Macau": {
-        "address": "Praça Ferreira do Amaral, Macau",
-        "radius": 1500,
-        "style": 1,
-        "rectangular": False,
-        "image": "macao.png",
-    },
-    "Palma Nova": {
-        "address": "Keerkring 49, 1705 Heerhugowaard, Netherlands",
-        "radius": 550,
-        "style": 1,
-        "rectangular": True,
-        "image": "heerhugowaard.png",
-    },
-    "Barcelona": "barcelona.png",
-    "Erbil": "erbil.png",
-    "Cidadebaixa": "bomfim-farroupilha-cidadebaixa.png",
-}
+
 if "settings" not in st.session_state:
     st.session_state.settings = EXAMPLES["Macau"]
 
---
+++ streamlit-prettybasicmaps/examples.py
@@ -0,0 +1,19 @@
+EXAMPLES = {
+    "Macau": {
+        "address": "Praça Ferreira do Amaral, Macau",
+        "radius": 1500,
+        "style": 1,
+        "rectangular": False,
+        "image": "macao.png",
+    },
+    "Palma Nova": {
+        "address": "Keerkring 49, 1705 Heerhugowaard, Netherlands",
+        "radius": 550,
+        "style": 1,
+        "rectangular": True,
+        "image": "heerhugowaard.png",
+    },
+    "Barcelona": "barcelona.png",
+    "Erbil": "erbil.png",
+    "Cidadebaixa": "bomfim-farroupilha-cidadebaixa.png",
+}
