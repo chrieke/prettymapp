bab512fdbfc4f703aed21b9876f33d04240fcf51
Christoph Rieke <christoph.rieke@up42.com>
2022-03-06 02:42:25 +0100

    pull out plot subfunctions

--- prettybasicmaps/plotting.py
+++ prettybasicmaps/plotting.py
@@ -9,27 +9,108 @@ from matplotlib.patches import Ellipse
 from matplotlib.pyplot import Rectangle
 
 
-def adjust_lightness(color, amount=0.5):
-    # To avoid having the user background ec color value which is similar to background color
+def adjust_lightness(color: str, amount=0.5):
+    """
+    Helper to avoid having the user background ec color value which is similar to background color.
+
+    via https://stackoverflow.com/questions/37765197/darken-or-lighten-a-color-in-matplotlib
+    """
+    #
     try:
         c = cnames[color]
-    except:
+    except KeyError:
         c = color
     c = colorsys.rgb_to_hls(*to_rgb(c))
     return colorsys.hls_to_rgb(c[0], max(0, min(1, amount * c[1])), c[2])
 
 
+def set_name(
+    ax,
+    bounds,
+    name: str,
+    font_size: int,
+    font_color: str,
+    text_x: int,
+    text_y: int,
+    text_rotation: int,
+):
+    xmin, ymin, xmax, ymax = bounds
+    xmid = (xmin + xmax) / 2
+    ymid = (ymin + ymax) / 2
+    xdif = xmax - xmin
+    ydif = ymax - ymin
+
+    x_text = xmid + text_x / 10 * xdif
+    y_text = ymid + text_y / 10 * ydif
+
+    _location_ = Path(__file__).resolve().parent
+    ax.text(
+        x=x_text,
+        y=y_text,
+        s=name,
+        color=font_color,
+        zorder=6,
+        ha="center",
+        rotation=text_rotation * -1,
+        fontproperties=fm.FontProperties(
+            fname=_location_ / "fonts/PermanentMarker-Regular.ttf"
+        ),
+        size=font_size,
+    )
+
+
+def set_background(ax, bounds, background_shape: str, background_color: str):
+    xmin, ymin, xmax, ymax = bounds
+    xmid = (xmin + xmax) / 2
+    ymid = (ymin + ymax) / 2
+    xdif = xmax - xmin
+    ydif = ymax - ymin
+
+    overhang = 0.02
+    if background_shape == "rectangle":
+        patch = Rectangle(
+            (xmin - (xdif * overhang), ymin - (ydif * overhang)),
+            xdif * (1 + 2 * overhang),
+            ydif * (1 + 2 * overhang),
+            color=background_color,
+            ec=adjust_lightness(background_color, 0.78),  # todo: correct value?
+            hatch="ooo...",
+            zorder=-1,
+            clip_on=True,
+        )
+        ax.add_patch(patch)
+    elif background_shape == "circle":
+        # axis aspect ratio no equal so ellipse required to display as circle
+        maxd = max(xdif, ydif)
+        width_ellipse = xdif / maxd * maxd * (1 + overhang)
+        height_ellipse = ydif / maxd * maxd * (1 + overhang)
+        ellipse = Ellipse(
+            (xmid, ymid),
+            width_ellipse,
+            height_ellipse,
+            facecolor=background_color,
+            ec=adjust_lightness(background_color, 0.78),
+            hatch="ooo...",
+            zorder=-1,
+            clip_on=True,
+        )
+        ax.add_artist(ellipse)
+    ax.patch.set_zorder(
+        -1
+    )  # re-enables patch for background color that is deactivated with axis
+
+
 def plot(
     df,
     drawing_kwargs,
-    name_on=False,
-    font_size=24,
-    font_color="#2F3737",
-    text_x=0,
-    text_y=0,
-    text_rotation=0,
-    background_shape="rectangle",
-    background_color="F2F4CB",
+    name_on: bool = False,
+    font_size: int = 24,
+    font_color: str = "#2F3737",
+    text_x: int = 0,
+    text_y: int = 0,
+    text_rotation: int = 0,
+    background_shape: str = "rectangle",
+    background_color: str = "F2F4CB",
 ):
     """
 
@@ -42,45 +123,15 @@ def plot(
     fig, ax = subplots(1, 1, figsize=(12, 12), constrained_layout=True, dpi=1200)
     ax.axis("off")
 
-    xmin, ymin, xmax, ymax = df.total_bounds
-    xmid = (xmin + xmax) / 2
-    ymid = (ymin + ymax) / 2
-    xdif = xmax - xmin
-    ydif = ymax - ymin
+    bounds = df.total_bounds
 
     if background_shape is not None:
-        overhang = 0.02
-        if background_shape == "rectangle":
-            patch = Rectangle(
-                (xmin - (xdif * overhang), ymin - (ydif * overhang)),
-                xdif * (1 + 2 * overhang),
-                ydif * (1 + 2 * overhang),
-                color=background_color,
-                ec=adjust_lightness(background_color, 0.78),  # todo: correct value?
-                hatch="ooo...",
-                zorder=-1,
-                clip_on=True,
-            )
-            ax.add_patch(patch)
-        elif background_shape == "circle":
-            # axis aspect ratio no equal so ellipse required to display as circle
-            maxd = max(xdif, ydif)
-            width_ellipse = xdif / maxd * maxd * (1 + overhang)
-            height_ellipse = ydif / maxd * maxd * (1 + overhang)
-            ellipse = Ellipse(
-                (xmid, ymid),
-                width_ellipse,
-                height_ellipse,
-                facecolor=background_color,
-                ec=adjust_lightness(background_color, 0.78),
-                hatch="ooo...",
-                zorder=-1,
-                clip_on=True,
-            )
-            ax.add_artist(ellipse)
-        ax.patch.set_zorder(
-            -1
-        )  # re-enables patch for background color that is deactivated with axis
+        set_background(
+            ax=ax,
+            bounds=bounds,
+            background_shape=background_shape,
+            background_color=background_color,
+        )
 
     for lc_class in df["landcover_class"].unique():
         df_class = df[df["landcover_class"] == lc_class]
@@ -102,22 +153,6 @@ def plot(
             df_class.plot(ax=ax, **draw_settings_class)
 
     if name_on:
-        x_text = xmid + text_x / 10 * xdif
-        y_text = ymid + text_y / 10 * ydif
-
-        _location_ = Path(__file__).resolve().parent
-        ax.text(
-            x=x_text,
-            y=y_text,
-            s="Stad van de Zon,\nHeerhugowaard, Netherlands",  # todo
-            color=font_color,
-            zorder=6,
-            ha="center",
-            rotation=text_rotation * -1,
-            fontproperties=fm.FontProperties(
-                fname=_location_ / "fonts/PermanentMarker-Regular.ttf"
-            ),
-            size=font_size,
-        )
+        set_name(ax, bounds, name, font_size, font_color, text_x, text_y, text_rotation)
 
     return fig
