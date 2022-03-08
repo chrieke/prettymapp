4cb1f29baa85db32480c0b3306595889727e88a0
Christoph Rieke <christoph.k.rieke@gmail.com>
2022-03-08 01:13:06 +0100

    rmv overwrite shorter var, doesnt work for flow

--- streamlit-prettybasicmaps/app.py
+++ streamlit-prettybasicmaps/app.py
@@ -1,6 +1,5 @@
 import streamlit as st
 from matplotlib.figure import Figure
-
 from examples import EXAMPLES
 from utils import image_button_config
 from prettybasicmaps.main import get_geometries
@@ -27,7 +26,6 @@ STYLE_OPTIONS = {
 
 if "settings" not in st.session_state:
     st.session_state.settings = EXAMPLES["Macau"]
-stss = st.session_state.settings
 
 st.set_page_config(
     page_title="prettybasicmaps", page_icon="🚀", initial_sidebar_state="collapsed"
@@ -41,22 +39,22 @@ example_cols = st.columns(5)
 for example_name, example_col in zip(EXAMPLES.keys(), example_cols):
     example_buttons.append(example_col.button(example_name))
 if any(example_buttons):
-    selected_example = EXAMPLES[list(EXAMPLES.keys())[example_buttons.index(True)]]
-    stss = selected_example
+    selected_example = list(EXAMPLES.keys())[example_buttons.index(True)]
+    st.session_state.settings = EXAMPLES[selected_example]
 st.write("")
 
 form = st.form(key="form_params")
 form.markdown("**Or choose your own location & map style**")
 col1, col2, col3 = form.columns([3, 1, 1])
 
-address = col1.text_input("Address or Location", stss["address"])
+address = col1.text_input("Address or Location", st.session_state.settings["address"])
 
 style = col2.selectbox(
     "Map Style",
     list(STYLE_OPTIONS.keys()),
-    list(STYLE_OPTIONS.keys()).index(stss["style"]),
+    list(STYLE_OPTIONS.keys()).index(st.session_state.settings["style"]),
 )
-radius = col3.slider("Radius Size", 1, 1500, stss["radius"])
+radius = col3.slider("Radius Size", 1, 1500, st.session_state.settings["radius"])
 
 
 expander = form.expander("More map style options")
@@ -65,25 +63,35 @@ shape_options = ["circle", "rectangle"]
 shape = col1style.radio(
     "Map Shape",
     options=shape_options,
-    index=shape_options.index(stss["shape"]),
+    index=shape_options.index(st.session_state.settings["shape"]),
 )
 bg_shape_options = ["rectangle", "circle", None]
 bg_shape = col1style.radio(
     "Background",
     options=bg_shape_options,
-    index=bg_shape_options.index(stss["bg_shape"]),
+    index=bg_shape_options.index(st.session_state.settings["bg_shape"]),
 )
-bg_color = col1style.color_picker("Background Color", stss["bg_color"])
+bg_color = col1style.color_picker(
+    "Background Color", st.session_state.settings["bg_color"]
+)
 
-name_on = col2style.checkbox("Add Location Name", stss["name_on"])
+name_on = col2style.checkbox("Add Location Name", st.session_state.settings["name_on"])
 custom_title = col2style.text_input(
-    "Use custom title instead", stss["custom_title"], max_chars=30
+    "Use custom title instead", st.session_state.settings["custom_title"], max_chars=30
 )
-font_size = col2style.slider("Font Size", 1, 50, stss["font_size"])
-font_color = col2style.color_picker("Font Color", stss["font_color"])
-text_x = col2style.slider("Text left/right", -100, 100, stss["text_x"])
-text_y = col2style.slider("Text top/bottom", -100, 100, stss["text_y"])
-text_rotation = col2style.slider("Text rotation", -90, 90, stss["text_rotation"])
+font_size = col2style.slider("Font Size", 1, 50, st.session_state.settings["font_size"])
+font_color = col2style.color_picker(
+    "Font Color", st.session_state.settings["font_color"]
+)
+text_x = col2style.slider(
+    "Text left/right", -100, 100, st.session_state.settings["text_x"]
+)
+text_y = col2style.slider(
+    "Text top/bottom", -100, 100, st.session_state.settings["text_y"]
+)
+text_rotation = col2style.slider(
+    "Text rotation", -90, 90, st.session_state.settings["text_rotation"]
+)
 
 vars = [
     address,
@@ -107,7 +115,7 @@ if submit_button:
     # TODO: second submit button click does not update session state?
     for var in vars:
         var_name = f"{var=}".split("=")[0]
-        stss[var_name] = var
+        st.session_state.settings[var_name] = var
 
 result_container = st.empty()
 with st.spinner("Creating new map...(may take up to a minute)"):
